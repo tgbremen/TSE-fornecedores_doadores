f03946bfccdaf25fffd9d5ad07be3bd9cf6083dd
tgbremen <83560989+tgbremen@users.noreply.github.com>
2022-09-23 16:24:45 -0300

    Primeira versão

---
+++ cpfs.py
@@ -0,0 +1,8 @@
+# Substituir os CPFs de exemplo por CPFs válidos. 
+# Os cpfs devem estar entre aspas simples ou duplas e separados por vírgulas, dentro dos colchetes. 
+# Não há limite para o número de CPFs
+cpfs = [ 
+'00000000001',
+'11111111112',
+'22222222222'
+]
